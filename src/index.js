a8464e0dabdf81e2b79ef8e1a694b75271c1d2e7
CentralCon <c-clarke@hotmail.com>
2025-03-11 08:27:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // React 18+ uses this for rendering
+import { BrowserRouter } from 'react-router-dom';
 import App from "./App"; // Importing the App component
 import './index.css'; // Importing styles if you need them
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App /> {/* Rendering the App component */}
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
